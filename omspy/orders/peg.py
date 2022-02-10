d8389a70c454b0908ec2f22ae810bf7254d2cecf
darkseid <uberdeveloper001@gmail.com>
2022-02-10 19:03:31 +0530

    peg to market order added

--- omspy/orders/peg.py
+++ omspy/orders/peg.py
@@ -1,21 +1,47 @@
 from typing import Optional, Dict, List, Type, Any, Union, Tuple, Callable
 from omspy.base import Broker
 from omspy.order import Order, CompoundOrder
+import pendulum
+
 
 class BasicPeg(CompoundOrder):
-    symbol:str
-    side:str
-    quantity:int
+    symbol: str
+    side: str
+    quantity: int
+    timezone: str = "UTC"
 
     def __init__(self, **data) -> None:
         super().__init__(**data)
-        order = Order(symbol=self.symbol, quantity=self.quantity,side=self.side, order_type='LIMIT')
+        order = Order(
+            symbol=self.symbol,
+            quantity=self.quantity,
+            side=self.side,
+            order_type="LIMIT",
+        )
         self.add(order)
         self.ltp[self.symbol] = 0
 
 
 class PegMarket(BasicPeg):
-    convert_to_market_after_expiry:bool = False
-    peg_every:int = 5
-    peg_limit:int = 10
+    duration: int = 60
+    peg_every: int = 10
+    convert_to_market_after_expiry: bool = False
+    _next_peg: Optional[pendulum.DateTime]
+    _num_pegs: int = 0
+    _max_pegs: int = 0
+    _expire_at: Optional[pendulum.DateTime]
 
+    def __init__(self, **data) -> None:
+        super().__init__(**data)
+        self._max_pegs = int(self.duration / self.peg_every)
+        self._num_pegs = 0
+        self._expire_at = pendulum.now(tz=self.timezone).add(seconds=self.duration)
+        self._next_peg = pendulum.now(tz=self.timezone).add(seconds=self.peg_every)
+
+    @property
+    def next_peg(self):
+        return self._next_peg
+
+    @property
+    def num_pegs(self):
+        return self._num_pegs
--- tests/orders/test_peg.py
+++ tests/orders/test_peg.py
@@ -1,12 +1,43 @@
 from omspy.orders.peg import *
 import pytest
 from omspy.brokers.paper import Paper
+import pendulum
+
 
 def test_basic_peg():
-    peg = BasicPeg(symbol='aapl', side='buy',
-            quantity=100, broker = Paper())
+    peg = BasicPeg(symbol="aapl", side="buy", quantity=100, broker=Paper())
     assert peg.count == 1
-    assert peg.orders[0].order_type == 'LIMIT'
-    assert peg.ltp['aapl'] == 0.0
+    assert peg.orders[0].order_type == "LIMIT"
+    assert peg.ltp["aapl"] == 0.0
 
 
+def test_peg_market_defaults():
+    known = pendulum.datetime(2022, 1, 1, 10)
+    with pendulum.test(known):
+        peg = PegMarket(symbol="aapl", side="buy", quantity=100, broker=Paper())
+        assert peg.num_pegs == 0
+        assert peg._max_pegs == 6
+        assert peg._expire_at == pendulum.datetime(2022, 1, 1, 10, minute=1)
+        assert peg.next_peg == pendulum.datetime(2022, 1, 1, 10, second=10)
+
+
+def test_peg_market_change_defaults():
+    known = pendulum.datetime(2022, 1, 1, 10, 5, 45, tz="Asia/Kolkata")
+    with pendulum.test(known):
+        peg = PegMarket(
+            symbol="aapl",
+            side="buy",
+            quantity=100,
+            broker=Paper(),
+            duration=150,
+            peg_every=20,
+            timezone="Asia/Kolkata",
+        )
+        assert peg.num_pegs == 0
+        assert peg._max_pegs == 7
+        assert peg._expire_at == pendulum.datetime(
+            2022, 1, 1, 10, minute=8, second=15, tz="Asia/Kolkata"
+        )
+        assert peg.next_peg == pendulum.datetime(
+            2022, 1, 1, 10, minute=6, second=5, tz="Asia/Kolkata"
+        )
