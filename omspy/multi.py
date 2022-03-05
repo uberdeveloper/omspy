68459df5cc9288b29af8fb65fbddf85c49b650a3
darkseid <uberdeveloper001@gmail.com>
2022-03-05 21:24:20 +0530

    order placement added for multi users

--- omspy/multi.py
+++ omspy/multi.py
@@ -3,42 +3,50 @@ Module for multi-user multi-broker implementation
 """
 from pydantic import BaseModel
 from omspy.base import Broker
-from typing import Dict,List,Optional
-
+from omspy.order import Order
+from typing import Dict, List, Optional
+from collections import defaultdict
 
 class User(BaseModel):
     """
     A basic user class for multi user environment
     """
-    broker:Broker
-    scale:float = 1.0
-    name:Optional[str]
-    exclude:Optional[Dict]
+
+    broker: Broker
+    scale: float = 1.0
+    name: Optional[str]
+    exclude: Optional[Dict]
 
     class Config:
         underscore_attrs_are_private = True
         arbitrary_types_allowed = True
-        
 
-class MultiBroker:
-    """
-    Multi-broker implementation
-    """
-    def __init__(self,users:List[User]):
-        self._users:List[User] = users
 
-    def add(self, user:User):
+class MultiUser:
+    """
+    Multi-userimplementation
+    """
+
+    def __init__(self, users: List[User]):
+        self._users: List[User] = users
+        self._orders: defaultdict(list) = {}
+
+    def add(self, user: User):
         """
         Add a user
         """
         self._users.append(user)
 
     @property
-    def users(self)->List[User]:
+    def users(self) -> List[User]:
         return self._users
 
     @property
-    def count(self)->int:
+    def orders(self)-> Dict[str,Order]:
+        return self._orders
+
+    @property
+    def count(self) -> int:
         return len(self.users)
 
     def _call(self, method, **kwargs):
@@ -46,3 +54,15 @@ class MultiBroker:
         Call the given method on all the users
         """
         pass
+    
+    def order_place(self, order:Order, **kwargs):
+        """
+        Place an order
+        """
+        self._orders[order.id] = []
+        for user in self.users:
+            order2 = order.clone()
+            order2.quantity = int(user.scale*order.quantity)
+            order2.parent_id = order2.pseudo_id = order.id
+            self._orders[order.id].append(order2)
+            order2.execute(user.broker, **kwargs)
--- tests/test_multi.py
+++ tests/test_multi.py
@@ -1,7 +1,8 @@
 from omspy.multi import *
+from omspy.order import Order
 from omspy.brokers.paper import Paper
 import pytest
-
+from unittest.mock import patch
 
 class Paper2(Paper):
     pass
@@ -24,18 +25,41 @@ def test_user_defaults():
     assert user.scale == 0.5
     assert user.name == 'mine'
 
-def test_multi_broker_defaults(users_simple):
-    multi = MultiBroker(users=users_simple)
+def test_multi_user_defaults(users_simple):
+    multi = MultiUser(users=users_simple)
     for user,expected in zip(multi.users,(1,0.5,2)):
         assert user.scale == expected
     multi.users[1].scale = 0.75
     assert multi.users[1].scale == 0.75
+    assert multi.orders == {}
 
-def test_multi_broker_add(users_simple):
-    multi = MultiBroker(users=users_simple)
+def test_multi_user_add(users_simple):
+    multi = MultiUser(users=users_simple)
     multi.add(User(broker=Paper(), scale=1.5, name='added'))
     assert multi.users[-1].scale == 1.5
     assert multi.users[-1].name == 'added'
     assert multi.count == 4
+
+def test_multi_user_order_place(users_simple):
+    multi = MultiUser(users=users_simple)
+    order = Order(symbol='aapl', side='buy', quantity=10)
+    multi.order_place(order)
+    orders = multi.orders.get(order.id)
+    expected_qty = [10,5,20]
+    for i in range(3):
+        assert orders[i].quantity ==  expected_qty[i]
+        assert orders[i].parent_id == order.id
+        assert orders[i].id != order.id
+
+def test_multi_user_order_place_broker(users_simple):
+    multi = MultiUser(users=users_simple)
+    with patch('omspy.brokers.paper.Paper') as order_place:
+        for user in multi.users:
+            # Patching  the order place method
+            user.broker.order_place = order_place
+    order = Order(symbol='aapl', side='buy', quantity=10)
+    multi.order_place(order)
+    assert order_place.call_count == 3
+
 
 
