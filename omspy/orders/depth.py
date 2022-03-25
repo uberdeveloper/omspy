bc6862888e30d5fc7032ab3182f18629b37ecae5
darkseid <uberdeveloper001@gmail.com>
2022-03-25 19:24:52 +0530

    ask,bid and sort methods added to market depth

--- omspy/orders/depth.py
+++ omspy/orders/depth.py
@@ -10,8 +10,28 @@ class MarketDepth(BaseModel):
     tick: float = 0.05
 
     @property
-    def midpoint(self):
+    def midpoint(self) -> float:
         a, b = self.bids[0].price, self.asks[0].price
         mp = abs(b - a) / 2
         tck = tick(min(a, b) + mp, 0.15)
         return round(tick(min(a, b) + mp, tick_size=self.tick), 2)
+
+    def bid(self, n: int = 0) -> float:
+        """
+        Get the bid price at the specified depth
+        """
+        return self.bids[n].price
+
+    def ask(self, n: int = 0) -> float:
+        """
+        Get the ask price at the specified depth
+        """
+        return self.asks[n].price
+
+    def sort(self):
+        """
+        Sort asks and bids based on price
+        sorting is done in place replacing asks and bids
+        """
+        self.bids.sort(key=lambda x: x.price, reverse=True)
+        self.asks.sort(key=lambda x: x.price)
--- tests/orders/test_depth.py
+++ tests/orders/test_depth.py
@@ -6,8 +6,16 @@ import pytest
 @pytest.fixture
 def simple_depth():
     return MarketDepth(
-        bids=[Quote(price=100, quantity=12, orders=4)],
-        asks=[Quote(price=101, quantity=12, orders=4)],
+        bids=[
+            Quote(price=100, quantity=54, orders=4),
+            Quote(price=99, quantity=1254, orders=12),
+            Quote(price=98, quantity=154, orders=4),
+        ],
+        asks=[
+            Quote(price=101, quantity=99, orders=4),
+            Quote(price=102, quantity=1288, orders=61),
+            Quote(price=103, quantity=359, orders=7),
+        ],
     )
 
 
@@ -21,3 +29,30 @@ def test_market_depth_midpoint(simple_depth):
     assert simple_depth.midpoint == 100.5
     simple_depth.tick = 0.07
     assert simple_depth.midpoint == 100.52
+
+
+def test_market_depth_bid(simple_depth):
+    assert simple_depth.bid() == 100
+    assert simple_depth.bid(2) == 98
+    assert simple_depth.bid(-1) == 98
+
+
+def test_market_depth_ask(simple_depth):
+    assert simple_depth.ask() == 101
+    assert simple_depth.ask(1) == 102
+    assert simple_depth.ask(-1) == 103
+
+
+def test_market_depth_sort(simple_depth):
+    simple_depth.bids.append(Quote(price=100.5, quantity=7))
+    simple_depth.asks.append(Quote(price=101.7, quantity=21))
+    assert simple_depth.bid(-1) == 100.5
+    assert simple_depth.bid(0) == 100
+    assert simple_depth.ask(-1) == 101.7
+    assert simple_depth.midpoint == 100.5
+    simple_depth.sort()
+    print(simple_depth.bids)
+    assert simple_depth.bid() == 100.5
+    assert simple_depth.bid(-1) == 98
+    assert simple_depth.ask(1) == 101.7
+    assert simple_depth.midpoint == 100.75
