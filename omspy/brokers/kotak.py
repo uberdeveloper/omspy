18103384d3273e0c6b7af9eab9d4cc4ddcceef8a
darkseid <uberdeveloper001@gmail.com>
2022-04-22 17:58:05 +0530

    naming functions for symbols added

--- omspy/brokers/kotak.py
+++ omspy/brokers/kotak.py
@@ -1,7 +1,8 @@
 from omspy.base import Broker,pre,post
-from typing import Optional,List,Dict
+from typing import Optional,List,Dict,Union
 from ks_api_client import ks_api
 import pendulum
+import pandas as pd
 
 def get_url(segment:Optional[str]='cash')->str:
     dt = pendulum.now(tz='Asia/Kolkata')
@@ -10,6 +11,68 @@ def get_url(segment:Optional[str]='cash')->str:
     seg = dct.get(segment, 'Cash')
     url = f'https://preferred.kotaksecurities.com/security/production/TradeApiInstruments_{seg}_{date_string}.txt'
     return url
+
+def get_name_for_cash_symbol(instrument_name:str, instrument_type:Optional[str]=None):
+    """
+    Get the broker interface name for a symbol in cash segment
+    """
+    if instrument_type is None:
+        pass
+    elif pd.isna(instrument_type):
+        instrument_type = None
+    elif not(instrument_type.isalnum()):
+        instrument_type = None
+    elif instrument_type.upper() == 'EQ':
+        instrument_type = None
+    elif instrument_type.lower() in ('na', 'nan'):
+        instrument_type = None
+
+    if instrument_type is None:
+        return instrument_name
+    else:
+        return f"{instrument_name}-{instrument_type or False}"
+
+def get_name_for_fno_symbol(instrument_name:str,expiry:Union[pendulum.Date, pendulum.DateTime, str],option_type:Optional[str]=None,strike:Optional[Union[float,int]]=None):
+    """
+    Get the broker interface name for a future or option symbol
+    Note
+    -----
+    1)If either the option type or option strike argument is not given, it is considered a futures contract
+    2) A contract is considered an option contract only if both option type and strike price is provided
+    """
+    if isinstance(expiry, str):
+        expiry = pendulum.parse(expiry)
+    expiry = expiry.strftime('%d%b%y').upper()
+
+    # Set the option type
+    opts = {'ce': 'call', 'pe': 'put'}
+    if option_type is None:
+        option_type = None
+    elif pd.isna(option_type):
+        option_type = None
+    elif not(isinstance(option_type, str)):
+        option_type = None
+    elif option_type.lower() not in ('ce', 'pe'):
+        option_type = None
+    else:
+        option_type = opts.get(option_type.lower())
+
+
+    # Set the strike
+    if strike is None:
+        strike = None
+    elif not(isinstance(strike, (int, float))):
+        strike = None
+    elif strike <= 0:
+        strike = None
+
+    # Consider an instrument futures if there is no
+    # corresponding strike or option type
+    if (option_type is None) or (strike is None):
+        return f"{instrument_name}{expiry}FUT".upper()
+    else:
+        return f"{instrument_name}{expiry}{strike}{option_type}".upper()
+
 
 
 class Kotak(Broker):
--- tests/brokers/test_kotak.py
+++ tests/brokers/test_kotak.py
@@ -24,3 +24,30 @@ def test_get_url(segment, date, expected):
 def test_get_url_no_segment():
     with pendulum.test(pendulum.datetime(2022,3,31)):
         assert get_url() == "https://preferred.kotaksecurities.com/security/production/TradeApiInstruments_Cash_31_03_2022.txt"
+
+@pytest.mark.parametrize("test_input, expected",
+        [
+            (('sbin', None), 'sbin'),
+            (('irfc', 'eq'), 'irfc'),
+            (('irfc', 'N1'), 'irfc-N1'),
+            (('bhel', 'na'), 'bhel'),
+            (('bhel', 'NAN'), 'bhel'),
+            (('TCS', '-'), 'TCS'),
+            (('TCS', pd.NA), 'TCS'),
+
+            ]
+        )
+def test_get_name_for_cash_symbol(test_input, expected):
+    assert get_name_for_cash_symbol(*test_input) == expected
+
+@pytest.mark.parametrize("test_input, expected",
+        [
+            (('nifty', '2022-05-20'),'NIFTY20MAY22FUT'),
+            (('nifty', pendulum.date(2021,7,13),'ce'),'NIFTY13JUL21FUT'),
+            (('nifty', pendulum.date(2021,7,13),'ce',14500),'NIFTY13JUL2114500CALL'),
+            (('nifty', pendulum.date(2021,7,13),'pe',14500),'NIFTY13JUL2114500PUT'),
+            (('sbin', pendulum.date(2021,7,13),'xx',14500),'SBIN13JUL21FUT'),
+            ]
+        )
+def test_get_name_for_fno_symbol(test_input, expected):
+    assert get_name_for_fno_symbol(*test_input) == expected
