85eb42063c852e6a123da78ec2a843bd7d41de1d
darkseid <uberdeveloper001@gmail.com>
2022-06-17 11:58:30 +0530

    more default arguments added to base strategy and straddle

--- omspy/algos/straddle.py
+++ omspy/algos/straddle.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel, ValidationError, validator
-from typing import List, Optional, Type, Tuple, Union
+from typing import List, Optional, Type, Tuple, Union, Dict
 from omspy.base import Broker
 from omspy.order import Order, CompoundOrder
 from omspy.models import Timer
+from omspy.orders.peg import PegExisting, PegSequential
 import pendulum
 import sqlite_utils
+import logging
 
 
 class BaseStrategy(BaseModel):
@@ -16,6 +18,7 @@ class BaseStrategy(BaseModel):
     connection: Optional[sqlite_utils.Database] = None
     timezone: Optional[str] = None
     _timer: Optional[Timer] = None
+    _pegs: List[Union[PegExisting, PegSequential]] = []
 
     class Config:
         arbitrary_types_allowed = True
@@ -31,30 +34,35 @@ class BaseStrategy(BaseModel):
     def timer(self):
         return self._timer
 
+
 class ShortStraddle(BaseStrategy):
-    symbols:Tuple[str,str]
-    limit_price:Optional[Tuple[float, float]] = (0.0,0.0)
-    trigger_price:Optional[Tuple[float, float]] = (0.0,0.0)
-    stop_price:Optional[Tuple[float, float]] = (0.0,0.0)
-    quantity:int = 1
-    exclude_stop:bool = False
+    symbols: Tuple[str, str]
+    limit_price: Optional[Tuple[float, float]] = (0.0, 0.0)
+    trigger_price: Optional[Tuple[float, float]] = (0.0, 0.0)
+    stop_price: Optional[Tuple[float, float]] = (0.0, 0.0)
+    quantity: int = 1
+    exclude_stop: bool = False
     _order: Optional[CompoundOrder] = None
+    _order_map: Optional[Dict[str, CompoundOrder]] = None
 
     def __init__(self, **data):
         super().__init__(**data)
-        self._order = CompoundOrder(broker=self.broker,
-                connection=self.connection, 
-                timezone= self.timezone)
+        self._order = CompoundOrder(
+            broker=self.broker, connection=self.connection, timezone=self.timezone
+        )
+        self._order_map = dict(entry1=None, exit1=None, entry2=None, exit2=None)
 
     @property
-    def order(self)->CompoundOrder:
+    def order(self) -> CompoundOrder:
         return self._order
+
+    def get_order(self, name: str):
+        pass
 
     def create_order(self):
         com = self._order
-        s1,s2 = self.symbols
-        order1 = Order(symbol=s1, side='sell', 
-                quantity = self.quantity)
+        s1, s2 = self.symbols
+        order1 = Order(symbol=s1, side="sell", quantity=self.quantity)
         order1.price = self.limit_price[0]
         order2 = order1.clone()
         order2.symbol = s2
@@ -64,17 +72,22 @@ class ShortStraddle(BaseStrategy):
         order1stop = order1.clone()
         order1stop.trigger_price = self.trigger_price[0]
         order1stop.price = self.stop_price[0]
-        order1stop.order_type = 'SL'
-        order1stop.side = 'buy'
+        order1stop.order_type = "SL"
+        order1stop.side = "buy"
         com.add(order1stop)
         order2stop = order2.clone()
         order2stop.trigger_price = self.trigger_price[1]
         order2stop.price = self.stop_price[1]
-        order2stop.order_type = 'SL'
-        order2stop.side = 'buy'
+        order2stop.order_type = "SL"
+        order2stop.side = "buy"
         com.add(order2stop)
         return self.order
 
-
-
-
+    def check_sell_both_sides(self) -> bool:
+        """
+        Check whether the sell order is placed on both the
+        sides and the orders are completed
+        """
+        orders = self.order.orders
+        if orders[0].is_complete and orders[1].is_complete:
+            return True
--- tests/algos/test_straddle.py
+++ tests/algos/test_straddle.py
@@ -2,16 +2,31 @@ import pytest
 from omspy.algos.straddle import *
 from pydantic import ValidationError
 
+
 @pytest.fixture
 def simple_straddle():
-    known = pendulum.datetime(2022,1,1)
+    known = pendulum.datetime(2022, 1, 1)
     with pendulum.test(known):
         return ShortStraddle(
-                start_time=pendulum.datetime(2022, 1, 1, 10, 10),
-                end_time=pendulum.datetime(2022, 1, 1, 15, 10),
-                symbols=('nifty22may17500ce', 'nifty22may17500pe'),
-                stop = (30,30)
-                )
+            start_time=pendulum.datetime(2022, 1, 1, 10, 10),
+            end_time=pendulum.datetime(2022, 1, 1, 15, 10),
+            symbols=("nifty22may17500ce", "nifty22may17500pe"),
+        )
+
+
+@pytest.fixture
+def price_straddle():
+    known = pendulum.datetime(2022, 1, 1)
+    with pendulum.test(known):
+        return ShortStraddle(
+            start_time=pendulum.datetime(2022, 1, 1, 10, 10),
+            end_time=pendulum.datetime(2022, 1, 1, 15, 10),
+            symbols=("nifty22may17500ce", "nifty22may17500pe"),
+            limit_price=(200, 210.4),
+            trigger_price=(240, 268),
+            stop_price=(242, 270),
+        )
+
 
 def test_base_strategy_defaults():
     known = pendulum.datetime(2022, 1, 1)
@@ -20,6 +35,9 @@ def test_base_strategy_defaults():
             start_time=pendulum.datetime(2022, 1, 1, 10, 10),
             end_time=pendulum.datetime(2022, 1, 1, 15, 10),
         )
+        assert base.timer.start_time == pendulum.datetime(2022, 1, 1, 10, 10)
+        assert base.timer.end_time == pendulum.datetime(2022, 1, 1, 15, 10)
+        assert base._pegs == []
 
 
 def test_base_strategy_end_time_less_than_start_time():
@@ -51,14 +69,40 @@ def test_base_strategy_time():
         assert base.timer.start_time == base.start_time
         assert base.timer.end_time == base.end_time
 
+
 def test_short_straddle_defaults(simple_straddle):
-    assert simple_straddle.start_time == pendulum.datetime(2022,1,1,10,10)
-    assert simple_straddle.symbols == ('nifty22may17500ce', 'nifty22may17500pe')
+    assert simple_straddle.start_time == pendulum.datetime(2022, 1, 1, 10, 10)
+    assert simple_straddle.symbols == ("nifty22may17500ce", "nifty22may17500pe")
+    assert simple_straddle.order is not None
+    assert list(simple_straddle._order_map.keys()) == [
+        "entry1",
+        "exit1",
+        "entry2",
+        "exit2",
+    ]
+    assert simple_straddle._pegs == []
+
 
 def test_short_straddle_create_order_defaults(simple_straddle):
     straddle = simple_straddle
     assert straddle.order.count == 0
     straddle.create_order()
     assert straddle.order.count == 4
-    assert [x.side for x in straddle.order.orders] == ['sell', 'sell', 'buy', 'buy']
+    assert [x.side for x in straddle.order.orders] == ["sell", "sell", "buy", "buy"]
+    assert [x.order_type for x in straddle.order.orders] == [
+        "MARKET",
+        "MARKET",
+        "SL",
+        "SL",
+    ]
 
+
+def test_short_straddle_limit_prices(price_straddle):
+    straddle = price_straddle
+    order = straddle.create_order()
+    assert order.orders[0].price == 200
+    assert order.orders[1].price == 210.4
+    assert order.orders[2].trigger_price == 240
+    assert order.orders[3].trigger_price == 268
+    assert order.orders[2].price == 242
+    assert order.orders[3].price == 270
