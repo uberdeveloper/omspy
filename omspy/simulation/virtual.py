555f2b5b5df0da65fe2c795994ebb4c630e4bb9a
darkseid <uberdeveloper001@gmail.com>
2023-01-13 21:06:38 +0530

    fake orderbook added

--- omspy/simulation/virtual.py
+++ omspy/simulation/virtual.py
@@ -1,5 +1,6 @@
 import random
 from typing import Optional
+from omspy.models import OrderBook, Quote
 
 
 def generate_price(start: int = 100, end: int = 110) -> int:
@@ -16,3 +17,51 @@ def generate_price(start: int = 100, end: int = 110) -> int:
     if start > end:
         start, end = end, start
     return random.randrange(start, end)
+
+
+def generate_orderbook(
+    bid: float = 100.0,
+    ask: float = 100.05,
+    depth: int = 5,
+    tick: float = 0.01,
+    quantity: int = 100,
+) -> OrderBook:
+    """
+    generate a fake orderbook
+    bid
+        bid price
+    ask
+        ask price
+    depth
+        depth of the orderbook
+    tick
+        difference in price between orders
+    quantity
+        average quantity of orders per price quote
+
+    Note
+    ----
+    1) orderbook is generated with a uniform tick difference between subsequent quotes
+    2) quantity is averaged between value quantity/2 and quantity * 1.5 using randrange function
+    3) num of orders is randomly picked between 5 to 15
+    4) if bid price is greater than ask, the values are swapped
+    """
+    if bid > ask:
+        bid, ask = ask, bid
+    asks = []
+    bids = []
+    q1, q2 = int(quantity * 0.5), int(quantity * 1.5)
+    for i in range(depth):
+        b = Quote(
+            price=bid - i * tick,
+            quantity=random.randrange(q1, q2),
+            orders_count=random.randrange(5, 15),
+        )
+        a = Quote(
+            price=bid + i * tick,
+            quantity=random.randrange(q1, q2),
+            orders_count=random.randrange(5, 15),
+        )
+        bids.append(b)
+        asks.append(a)
+    return OrderBook(ask=asks, bid=bids)
--- tests/simulation/test_virtual.py
+++ tests/simulation/test_virtual.py
@@ -9,3 +9,42 @@ def test_generate_price():
     assert generate_price() == 102
     assert generate_price(1000, 2000) == 1470
     assert generate_price(110, 100) == 107
+
+
+def test_generate_orderbook_default():
+    ob = generate_orderbook()
+    ob.bid[-1].price == 99.96
+    ob.ask[-1].price == 100.04
+    for b in ob.bid:
+        assert 50 < b.quantity < 150
+    for a in ob.ask:
+        assert 50 < b.quantity < 150
+
+
+def test_generate_orderbook_swap_bid_ask():
+    ob = generate_orderbook(bid=100.05, ask=100)
+    ob.bid[-1].price == 99.96
+    ob.ask[-1].price == 100.04
+    for b in ob.bid:
+        assert 50 < b.quantity < 150
+    for a in ob.ask:
+        assert 50 < b.quantity < 150
+
+
+def test_generate_orderbook_depth():
+    ob = generate_orderbook(depth=100)
+    ob.bid[-1].price == 99.01
+    ob.ask[-1].price == 100.99
+    assert len(ob.bid) == 100
+    assert len(ob.ask) == 100
+
+
+def test_generate_orderbook_price_and_tick_and_quantity():
+    ob = generate_orderbook(bid=1000, ask=1005, tick=2, depth=10, quantity=600)
+    ob.bid[-1].price == 982
+    ob.ask[-1].price == 1023
+    assert len(ob.bid) == len(ob.ask) == 10
+    for b in ob.bid:
+        assert 300 < b.quantity < 900
+    for a in ob.ask:
+        assert 300 < b.quantity < 900
