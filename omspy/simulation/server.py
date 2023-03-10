a0f00e709e90aba1a46098cceec8088d178cebb8
uberdeveloper <uberdeveloper001@gmail.com>
2023-03-10 20:10:16 +0530

    order_modify and order_cancel methods added

--- omspy/simulation/server.py
+++ omspy/simulation/server.py
@@ -6,15 +6,24 @@ from omspy.simulation.models import VOrder, OrderResponse, Side, Status
 
 app = FastAPI()
 app.broker: FakeBroker = FakeBroker()
+app._type = type(app.broker)
 
 
 class OrderArgs(BaseModel):
+    symbol: Optional[str]
+    side: Optional[Side]
+    quantity: Optional[int]
+    price: Optional[float]
+    trigger_price: Optional[float]
+    s: Optional[Status]
+
+
+class CreateArgs(BaseModel):
     symbol: str
     side: Side
     quantity: Optional[int]
     price: Optional[float]
     trigger_price: Optional[float]
-    s: Optional[Status]
 
 
 class ModifyArgs(BaseModel):
@@ -30,19 +39,24 @@ def read_root():
 
 @app.post("/order")
 async def create_order(order: OrderArgs) -> OrderResponse:
-    response = app.broker.order_place(**order.dict(exclude_none=True))
-
+    if app._type == FakeBroker:
+        response = app.broker.order_place(**order.dict(exclude_none=True))
     return OrderResponse(status="success", data=response)
 
 
 @app.put("/order/{order_id}")
-async def modify_order(order_id: str, order: ModifyArgs) -> OrderResponse:
-    response = app.broker.order_modify(order_id, **order.dict())
-    return response
+async def modify_order(order_id: str, order: OrderArgs) -> OrderResponse:
+    if app._type == FakeBroker:
+        response = app.broker.order_modify(
+            order_id=order_id, **order.dict(exclude_none=True)
+        )
+    return OrderResponse(status="success", data=response)
 
 
 @app.delete("/order/{order_id}")
-async def cancel_order(order_id: str) -> OrderResponse:
-    order = app.broker.order_cancel(order_id=order_id)
-    response = OrderResponse(status="success", data=order)
-    return response
+async def cancel_order(order_id: str, order: OrderArgs) -> OrderResponse:
+    if app._type == FakeBroker:
+        response = app.broker.order_cancel(
+            order_id=order_id, **order.dict(exclude_none=True)
+        )
+    return OrderResponse(status="success", data=response)
--- tests/simulation/test_server.py
+++ tests/simulation/test_server.py
@@ -62,3 +62,48 @@ def test_order_status_open():
     assert r["quantity"] == 100
     assert r["filled_quantity"] == r["canceled_quantity"] == 0
     assert r["pending_quantity"] == 100
+
+
+def test_order_modify():
+    data = dict(price=120, quantity=125)
+    response = client.put("/order/abcd1234", json=data)
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    r = response.json()["data"]
+    assert r["price"] == 120
+    assert r["quantity"] == 125
+
+
+def test_order_modify_other_args():
+    data = dict(price=120, quantity=125, symbol="abcd", side=-1, s=2)
+    response = client.put("/order/abcd1234", json=data)
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    r = response.json()["data"]
+    assert r["order_id"] == "abcd1234"
+    assert r["symbol"] == "abcd"
+    assert r["side"] == -1
+    assert r["pending_quantity"] == 125
+    assert r["canceled_quantity"] == r["filled_quantity"] == 0
+
+
+def test_order_cancel():
+    response = client.request(method="delete", url="/order/abcd1234", json=dict())
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    r = response.json()["data"]
+    assert r["order_id"] == "abcd1234"
+    assert r["canceled_quantity"] == r["quantity"]
+
+
+def test_order_cancel_other_args():
+    data = dict(price=120, quantity=1000, symbol="abcd", side=-1, s=1)
+    response = client.request(method="delete", url="/order/abcd1234", json=data)
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    r = response.json()["data"]
+    assert r["order_id"] == "abcd1234"
+    assert r["symbol"] == "abcd"
+    assert r["side"] == -1
+    assert r["pending_quantity"] == r["filled_quantity"] == 0
+    assert r["canceled_quantity"] == 1000
