da19cf319a0af076f8d99da00a098e21b23628b9
darkseid <uberdeveloper001@gmail.com>
2024-10-30 00:43:40 +0530

    orders with status and positions

--- omspy/brokers/icici.py
+++ omspy/brokers/icici.py
@@ -6,6 +6,7 @@ import logging
 import nodriver as uc
 import time
 from breeze_connect import BreezeConnect
+import pendulum
 
 
 class Icici(Broker):
@@ -42,55 +43,117 @@ class Icici(Broker):
         return details
 
     @pre
-    def order_place(self, **kwargs)->dict:
+    def order_place(self, **kwargs) -> Optional[str]:
         """
         Place an order
         """
         symbol = kwargs.pop("stock_code")
         order_type = kwargs.pop("order_type", "MARKET")
         if "stop" in order_type.lower():
-            order_type = "stoploss"
+            order_type = "LIMIT"
         elif "sl" in order_type.lower():
-            order_type = "stoploss"
+            order_type = "LIMIT"
 
         name = self.breeze.get_names(exchange_code="NSE", stock_code=symbol)
         order_args = dict(
-                validity="day",
-                product="margin",
-                stock_code=name['isec_stock_code'],
-                exchange_code="NSE",
-                order_type=order_type,
-                )
+            validity="day",
+            product="margin",
+            stock_code=name["isec_stock_code"],
+            exchange_code="NSE",
+            order_type=order_type,
+        )
         order_args.update(kwargs)
-        return self.breeze.place_order(**order_args)
+        response = self.breeze.place_order(**order_args)
+        success = response["Success"]
+        if success and "order_id" in success:
+            return success["order_id"]
+        else:
+            return None
 
     @pre
-    def order_modify(self, order_id:str, **kwargs)->dict:
+    def order_modify(self, order_id: str, **kwargs) -> Optional[str]:
         """
         Modify an existing order
         """
         if "price" in kwargs:
-            order_type = "LIMIT"
+            if kwargs["price"] == 0:
+                order_type = "MARKET"
+            else:
+                order_type = "LIMIT"
         elif "stoploss" in kwargs:
-            order_type = "stoploss"
+            order_type = "LIMIT"
+        elif "sl" in kwargs:
+            order_type = "LIMIT"
         else:
-            order_type ="LIMIT"
+            order_type = "MARKET"
         order_args = dict(
-                validity="day",
-                exchange_code="NSE",
-                order_id=order_id,
-                order_type=order_type,
-                )
+            validity="day",
+            exchange_code="NSE",
+            order_id=order_id,
+            order_type=order_type,
+        )
         order_args.update(kwargs)
-        return self.breeze.modify_order(**order_args)
+        response = self.breeze.modify_order(**order_args)
+        success = response["Success"]
+        if success and "order_id" in success:
+            return success["order_id"]
+        else:
+            return None
 
-
-    def order_cancel(self, order_id:str, **kwargs)->dict:
+    def order_cancel(self, order_id: str, **kwargs) -> Optional[str]:
         """
         Cancel an existing order
         """
         order_args = dict(
-                exchange_code="NSE",
-                )
+            exchange_code="NSE",
+        )
         order_args.update(kwargs)
-        return self.breeze.cancel_order(order_id=order_id, **order_args)
+        response = self.breeze.cancel_order(order_id=order_id, **order_args)
+        success = response["Success"]
+        if success and "order_id" in success:
+            return success["order_id"]
+        else:
+            return None
+
+    @property
+    @post
+    def orders(self) -> List[Optional[Dict]]:
+        """
+        Return all the orders
+        """
+        tz = "Asia/Kolkata"
+        from_date = str(pendulum.today(tz))
+        to_date = str(pendulum.now(tz))
+        response = self.breeze.get_order_list(
+            exchange_code="NSE", from_date=from_date, to_date=to_date
+        )
+        success = response["Success"]
+        if isinstance(success, list):
+            orderbook = success
+        else:
+            orderbook = []
+        status_map = {
+            "EXECUTED": "COMPLETE",
+            "ORDERED": "PENDING",
+            "CANCELLED": "CANCELED",
+            "REJECTED": "CANCELED",
+        }
+        if len(orderbook) > 0:
+            for order in orderbook:
+                status = str(order["status"]).upper()
+                order["status"] = status_map.get(status, "PENDING")
+        return orderbook
+
+    @property
+    @post
+    def positions(self) -> List[Optional[Dict]]:
+        """
+        Return all the positions
+        """
+        response = self.breeze.get_portfolio_positions()
+        success = response["Success"]
+        if isinstance(success, list):
+            positions = success
+        else:
+            positions = []
+        return positions
