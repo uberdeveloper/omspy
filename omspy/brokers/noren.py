71a5412854ec5199b1fe18a2da8f340c2f55eee9
darkseid <uberdeveloper001@gmail.com>
2024-12-04 11:03:44 +0530

    order placement added

--- omspy/brokers/noren.py
+++ omspy/brokers/noren.py
@@ -11,7 +11,7 @@ class BaseNoren(NorenApi):
         super(BaseNoren, self).__init__(host=host, websocket=websocket)
 
 
-class Noren:
+class Noren(Broker):
     def __init__(
         self,
         user_id: str,
@@ -21,7 +21,7 @@ class Noren:
         app_key: str,
         imei: str,
         *args,
-        **kwargs
+        **kwargs,
     ):
         self._user_id = user_id
         self._password = password
@@ -32,6 +32,7 @@ class Noren:
         self._host = kwargs.get("host", "https://star.prostocks.com/NorenWClientTP")
         self._websocket = kwargs.get("websocket", "wss://star.prostocks.com/NorenWS/")
         self.noren = None
+        super(Noren, self).__init__()
 
     @property
     def attribs_to_copy_modify(self) -> set:
@@ -50,3 +51,89 @@ class Noren:
     def authenticate(self) -> Union[Dict, None]:
         self.noren = BaseNoren(self._host, self._websocket)
         return self.login()
+
+    def _convert_symbol(self, symbol: str, exchange: str = "NSE") -> str:
+        """
+        Convert raw symbol to noren
+        """
+        if exchange == "NSE":
+            if symbol.upper().endswith("-EQ"):
+                return symbol
+            else:
+                return f"{symbol}-EQ"
+        else:
+            return symbol
+
+    def get_order_type(self, order_type: str) -> str:
+        """
+        Convert a generic order type to this specific
+        broker's order type string
+        returns MKT if the order_type is not matching
+        """
+        order_types = dict(
+            LIMIT="LMT",
+            MARKET="MKT",
+            SL="SL-LMT",
+            SLM="SL-MKT",
+            SLL="SL-LMT",
+            LMT="LMT",
+            MKT="MKT",
+        )
+        order_types["SL-M"] = "SL-MKT"
+        order_types["SL-L"] = "SL-LMT"
+        return order_types.get(order_type.upper(), "MKT")
+
+    @pre
+    def order_place(self, **kwargs) -> Union[str, None]:
+        side = kwargs.pop("buy_or_sell")
+        order_type = kwargs.pop("price_type", "MKT")
+        exchange = kwargs.pop("exchange", "NSE")
+        symbol = kwargs.pop("tradingsymbol")
+        symbol = self._convert_symbol(symbol, exchange=exchange)
+        if order_type:
+            order_type = self.get_order_type(order_type)
+        if side:
+            side = side.upper()[0]
+        if symbol:
+            symbol = symbol.upper()
+        order_args = dict(
+            tradingsymbol=symbol,
+            buy_or_sell=side,
+            price_type=order_type,
+            exchange=exchange,
+            retention="DAY",
+            product_type="I",
+            discloseqty=0,
+        )
+        order_args.update(kwargs)
+        response = self.noren.place_order(**order_args)
+        return response.get("norenordno")
+
+    @pre
+    def order_modify(self, **kwargs) -> Union[str, None]:
+        """
+        Modify an existing order
+        """
+        symbol = kwargs.pop("tradingsymbol")
+        order_id = kwargs.pop("order_id", None)
+        order_type = kwargs.pop("order_type", "MKT")
+        if "discloseqty" in kwargs:
+            kwargs.pop("discloseqty")
+        if order_type:
+            order_type = self.get_order_type(order_type)
+        if symbol:
+            symbol = self._convert_symbol(symbol).upper()
+        order_args = dict(
+            orderno=order_id,
+            newprice_type=order_type,
+            exchange="NSE",
+            tradingsymbol=symbol,
+        )
+        order_args.update(kwargs)
+        return self.noren.modify_order(**order_args)
+
+    def order_cancel(self, order_id: str) -> Union[Dict, None]:
+        """
+        Cancel an existing order
+        """
+        return self.noren.cancel_order(orderno=order_id)
--- tests/brokers/test_noren.py
+++ tests/brokers/test_noren.py
@@ -64,3 +64,140 @@ def test_authenticate():
         )
         broker.authenticate()
         broker.noren.login.assert_called_once()
+
+
+def test_get_order_type(broker):
+    assert broker.get_order_type("limit") == "LMT"
+    assert broker.get_order_type("SLM") == "SL-MKT"
+    assert broker.get_order_type("STOP") == "MKT"
+    assert broker.get_order_type("SLL") == "SL-LMT"
+    assert broker.get_order_type("SLL") == "SL-LMT"
+    assert broker.get_order_type("SL-L") == "SL-LMT"
+    assert broker.get_order_type("SL-M") == "SL-MKT"
+
+
+def test_order_place(broker):
+    broker.order_place(symbol="reliance-eq", side="buy", quantity=1)
+    broker.noren.place_order.assert_called_once()
+    order_args = dict(
+        buy_or_sell="B",
+        product_type="I",
+        exchange="NSE",
+        tradingsymbol="RELIANCE-EQ",
+        quantity=1,
+        price_type="MKT",
+        retention="DAY",
+        discloseqty=0,
+    )
+    assert broker.noren.place_order.call_args.kwargs == order_args
+
+
+def test_order_place_change_args(broker):
+    broker.order_place(
+        symbol="reliance-eq",
+        side="buy",
+        quantity=100,
+        validity="DAY",
+        order_type="limit",
+        price=2100,
+    )
+    broker.noren.place_order.assert_called_once()
+    order_args = dict(
+        buy_or_sell="B",
+        product_type="I",
+        exchange="NSE",
+        tradingsymbol="RELIANCE-EQ",
+        quantity=100,
+        price_type="LMT",
+        price=2100,
+        retention="DAY",
+        discloseqty=0,
+    )
+    assert broker.noren.place_order.call_args.kwargs == order_args
+
+
+def test_order_place_mixed_args(broker):
+    broker.order_place(
+        symbol="reliance-eq",
+        side="buy",
+        quantity=100,
+        validity="DAY",
+        price_type="LMT",
+        price=2100,
+        disclosed_quantity=15,
+        product_type="N",
+    )
+    broker.noren.place_order.assert_called_once()
+    order_args = dict(
+        buy_or_sell="B",
+        product_type="N",
+        exchange="NSE",
+        tradingsymbol="RELIANCE-EQ",
+        quantity=100,
+        price_type="LMT",
+        price=2100,
+        retention="DAY",
+        discloseqty=15,
+    )
+    assert broker.noren.place_order.call_args.kwargs == order_args
+
+
+def test_modify_order(broker):
+    broker.order_modify(symbol="RELIANCE-EQ", quantity=5, order_id=1234)
+    broker.noren.modify_order.assert_called_once()
+    order_args = dict(
+        orderno=1234,
+        tradingsymbol="RELIANCE-EQ",
+        newquantity=5,
+        newprice_type="MKT",
+        exchange="NSE",
+    )
+    assert broker.noren.modify_order.call_args.kwargs == order_args
+
+
+def test_modify_order_kwargs(broker):
+    broker.order_modify(
+        symbol="RELIANCE-EQ",
+        quantity=5,
+        order_id="1234",
+        exchange="NSE",
+        order_type="limit",
+        price=2075,
+    )
+    broker.noren.modify_order.assert_called_once()
+    order_args = dict(
+        orderno="1234",
+        tradingsymbol="RELIANCE-EQ",
+        newquantity=5,
+        newprice_type="LMT",
+        exchange="NSE",
+        newprice=2075,
+    )
+    assert broker.noren.modify_order.call_args.kwargs == order_args
+
+
+def test_cancel_order(broker):
+    broker.order_cancel(order_id=1234)
+    broker.noren.cancel_order.assert_called_once()
+
+
+def test_convert_symbol(broker):
+    assert broker._convert_symbol("reliance", "RELIANCE-EQ")
+    assert broker._convert_symbol("RELIANCE-EQ", "RELIANCE-EQ")
+    assert broker._convert_symbol("reliance-eq", "RELIANCE-EQ")
+
+
+def test_place_order_without_eq(broker):
+    broker.order_place(symbol="RELIANCE", side="BUY", quantity=1)
+    broker.noren.place_order.assert_called_once()
+    order_args = dict(
+        buy_or_sell="B",
+        product_type="I",
+        exchange="NSE",
+        tradingsymbol="RELIANCE-EQ",
+        quantity=1,
+        price_type="MKT",
+        retention="DAY",
+        discloseqty=0,
+    )
+    assert broker.noren.place_order.call_args.kwargs == order_args
